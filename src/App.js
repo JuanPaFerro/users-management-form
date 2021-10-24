1af04916019874913c96e43a1385e7431bce2291
unknown <juanpabloferroarroyo@gmail.com>
2021-10-24 23:11:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import useForm from "./hooks/useForm";
+import Input from "./components/Input";
 
 function App() {
+  const [form, handleChange] = useForm({ name: "", lastname: "" });
+  console.log(form);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form action="">
+      <Input
+        name="name"
+        label="Name"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <Input
+        name="lastname"
+        label="Last"
+        value={form.lastname}
+        onChange={handleChange}
+      />
+    </form>
   );
 }
 
